8665088a98426b106b8cb359d649aaefaa1c8a64
tk0miya <i.tkomiya@gmail.com>
2011-04-06 23:23:52 +0900

    * Implement prototype of netdiag

---
+++ src/netdiag/elements.py
@@ -0,0 +1,16 @@
+#!bin/py
+# -*- coding: utf-8 -*-
+
+import re
+import sys
+import blockdiag.elements
+from blockdiag.elements import *
+from blockdiag.utils.XY import XY
+
+
+class Diagram(blockdiag.elements.Diagram):
+    def __init__(self):
+        super(Diagram, self).__init__()
+
+        self.orientation = 'portrait'
+        self.span_height = 120;
