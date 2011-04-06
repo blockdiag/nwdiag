e19c90c10009d6ccb8d607f259b83979670b0d4d
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-04-06 21:36:00 +0900

    * Add project files for packaging

---
+++ setup.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+import os, sys
+
+version = '0.1.0'
+long_description = \
+        open(os.path.join("src","README.txt")).read() + \
+        open(os.path.join("src","TODO.txt")).read()
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: System Administrators",
+    "License :: OSI Approved :: Python Software Foundation License",
+    "Programming Language :: Python",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Documentation",
+    "Topic :: Text Processing :: Markup",
+]
+
+setup(
+     name='netdiag',
+     version=version,
+     description='netdiag generate network-diagram image file from spec-text file.',
+     long_description=long_description,
+     classifiers=classifiers,
+     keywords=['diagram','generator'],
+     author='Takeshi Komiya',
+     author_email='i.tkomiya at gmail.com',
+     url='http://tk0miya.bitbucket.org/netdiag/build/html/index.html',
+     license='PSL',
+     py_modules=['netdiag_sphinxhelper'],
+     packages=find_packages('src'),
+     package_dir={'': 'src'},
+     package_data = {'': ['buildout.cfg']},
+     include_package_data=True,
+     install_requires=[
+        'setuptools',
+        'PIL',
+        'funcparserlib',
+         # -*- Extra requirements: -*-
+     ],
+     extras_require=dict(
+         test=[
+             'Nose',
+             'minimock',
+             'pep8',
+         ],
+         pdf=[
+             'reportlab',
+         ],
+     ),
+     test_suite='nose.collector',
+     tests_require=['Nose','minimock','pep8'],
+     entry_points="""
+        [console_scripts]
+        netdiag = netdiag:main
+     """,
+)
+
