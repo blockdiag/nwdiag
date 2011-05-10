39cfadff12d1287fcabc31248217c8664cb448f6
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-05-10 12:16:58 +0900

    * Refactored

--- src/nwdiag/DiagramDraw.py
+++ src/nwdiag/DiagramDraw.py
@@ -52,55 +52,37 @@ class DiagramDraw(blockdiag.DiagramDraw.DiagramDraw):
 
     def node(self, node, **kwargs):
         m = self.metrix
-        pt0 = m.node(node).top()
-        pt1 = XY(pt0.x, pt0.y - m.spanHeight / 3)
 
         i = 0
         textbox_width = m.nodeWidth + m.spanWidth
         node.networks.sort(lambda a, b: cmp(a.xy.y, b.xy.y))
         for network in node.networks:
-            jumps = []
             if network.xy.y == node.xy.y:
-                x, y = m.cell(node).top()
+                x, y2 = m.node(node).top()
+                y1 = m.network(network).top().y
 
-                y0 = m.cell(network).top().y - m.spanHeight / 2
-                textbox = [x, y0, x + textbox_width, y]
+                x_shift = 0
+                textbox = [x, y1, x + textbox_width, y2]
             else:
-                x, y = m.cell(node).bottom()
+                x, y1 = m.node(node).bottom()
+                y2 = m.network(network).top().y
+
                 num = len(node.networks) - 2
                 x_shift = int(math.floor(num / 2.0 - i)) * m.cellSize * 2
-                x += x_shift
-
-                y0 = m.cell(network).top().y - m.spanHeight / 2
-                textbox = [x, y0 - m.spanHeight / 2,
-                           x + textbox_width - x_shift, y0]
 
                 i += 1
 
-                for j in range(node.xy.y + 1, network.xy.y):
-                    crosses = (n for n in self.diagram.networks if n.xy.y == j)
-                    for nw in crosses:
-                        if nw.xy.x <= node.xy.x <= nw.xy.x + nw.width:
-                            jumps.append(nw)
+            textbox = [x + x_shift, y2 - m.spanHeight / 2, x + textbox_width, y2]
+            x += x_shift
 
-            if jumps:
-                lines = [XY(x, y)]
-                for nw in jumps:
-                    pt = m.cell(nw).top().y - m.spanHeight / 2
-                    r = m.cellSize / 2
+            jumps = []
+            for j in range(node.xy.y + 1, network.xy.y):
+                crosses = (n for n in self.diagram.networks if n.xy.y == j)
+                for nw in crosses:
+                    if nw.xy.x <= node.xy.x <= nw.xy.x + nw.width:
+                        jumps.append(nw)
 
-                    lines.append(XY(x, pt - r))
-                    lines.append(XY(x, pt + r))
-
-                    box = [x - r, pt - r, x + r, pt + r]
-                    self.drawer.arc(box, 270, 90, fill=self.fill)
-
-                lines.append(XY(x, y0))
-
-                for j, point in enumerate(lines[::2]):
-                    self.drawer.line([point, lines[j * 2 + 1]], fill=self.fill)
-            else:
-                self.drawer.line([XY(x, y0), XY(x, y)], fill=self.fill)
+            self.draw_connectors([XY(x, y1), XY(x, y2)], jumps)
 
             if network in node.address:
                 label = node.address[network]
@@ -109,6 +91,29 @@ class DiagramDraw(blockdiag.DiagramDraw.DiagramDraw):
                                      fontsize=self.metrix.fontSize)
 
         super(DiagramDraw, self).node(node, **kwargs)
+
+    def draw_connectors(self, line, jumps):
+        m = self.metrix
+
+        if jumps:
+            pt1, pt2 = line
+            lines = [pt1]
+            for nw in jumps:
+                pt = m.cell(nw).top().y - m.spanHeight / 2
+                r = m.cellSize / 2
+
+                lines.append(XY(pt1.x, pt - r))
+                lines.append(XY(pt1.x, pt + r))
+
+                box = [pt1.x - r, pt - r, pt1.x + r, pt + r]
+                self.drawer.arc(box, 270, 90, fill=self.fill)
+
+            lines.append(pt2)
+
+            for j, point in enumerate(lines[::2]):
+                self.drawer.line([point, lines[j * 2 + 1]], fill=self.fill)
+        else:
+            self.drawer.line(line, fill=self.fill)
 
 
 from DiagramMetrix import DiagramMetrix
