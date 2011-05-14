189c828d32a79cfc31414568093fb01ec3c820e4
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-05-14 15:14:20 +0900

    * Update version

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
         'setuptools',
         'PIL',
         'funcparserlib',
-        'blockdiag',
+        'blockdiag>=0.8.1',
          # -*- Extra requirements: -*-
      ],
      extras_require=dict(
