3e1d95728395755b6b900e45a910774664583d0b
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2011-11-06 19:12:40 +0900

    Regenerate examples

--- examples/update.sh
+++ examples/update.sh
@@ -5,11 +5,11 @@ NWDIAG=$EXAMPLES/../bin/nwdiag
 
 for diag in `ls $EXAMPLES/nwdiag/*.diag`
 do
-    png=$EXAMPLES/`basename $diag .diag`.png
+    png=$EXAMPLES/nwdiag/`basename $diag .diag`.png
     echo $NWDIAG -Tpng -o $png $diag
     $NWDIAG -Tpng -o $png $diag
 
-    svg=$EXAMPLES/`basename $diag .diag`.svg
+    svg=$EXAMPLES/nwdiag/`basename $diag .diag`.svg
     echo $NWDIAG -Tsvg -o $svg $diag
     $NWDIAG -Tsvg -o $svg $diag
 done
