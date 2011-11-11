be515ce8f4c4d2a3c51b6b569e68b59f7dd16804
tk0miya <i.tkomiya@gmail.com>
2011-11-11 17:57:59 +0900

    Remove ad-hoc code and depend blockdiag >= 1.0.3

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
      include_package_data=True,
      install_requires=[
         'setuptools',
-        'blockdiag>=1.0.2',
+        'blockdiag>=1.0.3',
          # -*- Extra requirements: -*-
      ],
      extras_require=dict(
