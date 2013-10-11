528afda48f90777c0936e600a9f794ff92250cbe
tk0miya <i.tkomiya@gmail.com>
2013-10-11 12:21:02 +0900

    Fix flake8 violations

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+import os
+import sys
 from setuptools import setup, find_packages
-import os, sys
 
 sys.path.insert(0, 'src')
 import nwdiag
 
-long_description = open(os.path.join("src","README.txt")).read()
+long_description = open(os.path.join("src", "README.txt")).read()
 
 classifiers = [
     "Development Status :: 3 - Alpha",
@@ -16,55 +17,56 @@ classifiers = [
     "Topic :: Software Development :: Documentation",
     "Topic :: Text Processing :: Markup",
 ]
-test_requires = [
-             'Nose',
-             'pep8>=1.3',]
+test_requires = ['Nose', 'pep8>=1.3']
 
 # only for Python2.6
 if sys.version_info > (2, 6) and sys.version_info < (2, 7):
     test_requires.append('unittest2')
 
 setup(
-     name='nwdiag',
-     version=nwdiag.__version__,
-     description='nwdiag generate network-diagram image file from spec-text file.',
-     long_description=long_description,
-     classifiers=classifiers,
-     keywords=['diagram','generator'],
-     author='Takeshi Komiya',
-     author_email='i.tkomiya at gmail.com',
-     url='http://blockdiag.com/',
-     download_url='http://pypi.python.org/pypi/nwdiag',
-     license='Apache License 2.0',
-     py_modules=['nwdiag_sphinxhelper', 'rackdiag_sphinxhelper', 'packetdiag_sphinxhelper'],
-     packages=find_packages('src'),
-     package_dir={'': 'src'},
-     package_data = {'': ['buildout.cfg']},
-     include_package_data=True,
-     install_requires=[
+    name='nwdiag',
+    version=nwdiag.__version__,
+    description='nwdiag generates network-diagram image from text',
+    long_description=long_description,
+    classifiers=classifiers,
+    keywords=['diagram', 'generator'],
+    author='Takeshi Komiya',
+    author_email='i.tkomiya at gmail.com',
+    url='http://blockdiag.com/',
+    download_url='http://pypi.python.org/pypi/nwdiag',
+    license='Apache License 2.0',
+    py_modules=[
+        'nwdiag_sphinxhelper',
+        'rackdiag_sphinxhelper',
+        'packetdiag_sphinxhelper',
+    ],
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    package_data={'': ['buildout.cfg']},
+    include_package_data=True,
+    install_requires=[
         'setuptools',
         'blockdiag>=1.3.0',
-         # -*- Extra requirements: -*-
-     ],
-     extras_require=dict(
-         test=test_requires,
-         pdf=[
-             'reportlab',
-         ],
-         rst=[
-             'docutils',
-         ],
-     ),
-     test_suite='nose.collector',
-     tests_require=test_requires,
-     entry_points="""
-        [console_scripts]
-        nwdiag = nwdiag.command:main
-        rackdiag = rackdiag.command:main
-        packetdiag = packetdiag.command:main
+        # -*- Extra requirements: -*-
+    ],
+    extras_require=dict(
+        test=test_requires,
+        pdf=[
+            'reportlab',
+        ],
+        rst=[
+            'docutils',
+        ],
+    ),
+    test_suite='nose.collector',
+    tests_require=test_requires,
+    entry_points="""
+       [console_scripts]
+       nwdiag = nwdiag.command:main
+       rackdiag = rackdiag.command:main
+       packetdiag = packetdiag.command:main
 
-        [blockdiag_noderenderer]
-        _packet_node = packetdiag.noderenderers
-     """,
+       [blockdiag_noderenderer]
+       _packet_node = packetdiag.noderenderers
+    """,
 )
-
