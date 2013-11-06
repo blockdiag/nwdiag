0ab27f1e42a97fad847d3f2f9fec6e36ea01a623
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2013-11-06 23:06:03 +0900

    Refactor rackdiag.parser

--- src/rackdiag/parser.py
+++ src/rackdiag/parser.py
@@ -41,16 +41,15 @@ from re import MULTILINE, DOTALL
 from collections import namedtuple
 from funcparserlib.lexer import make_tokenizer, Token, LexerError
 from funcparserlib.parser import (some, a, maybe, many, finished, skip)
+from blockdiag.parser import create_mapper, oneplus_to_list
 from blockdiag.utils.compat import u
 
-ENCODING = 'utf-8'
 
-Graph = namedtuple('Graph', 'type id stmts')
+Diagram = namedtuple('Diagram', 'type id stmts')
 RackItem = namedtuple('RackItem', 'number label attrs')
 Attr = namedtuple('Attr', 'name value')
 Rack = namedtuple('Rack', 'id stmts')
-DefAttrs = namedtuple('DefAttrs', 'object attrs')
-AttrPlugin = namedtuple('AttrPlugin', 'name attrs')
+Extension = namedtuple('Extension', 'type name attrs')
 
 
 class ParseException(Exception):
@@ -60,19 +59,18 @@ class ParseException(Exception):
 def tokenize(string):
     """str -> Sequence(Token)"""
     # flake8: NOQA
-    specs = [                                                                  # NOQA
-        ('Comment',        (r'/\*(.|[\r\n])*?\*/', MULTILINE)),                # NOQA
-        ('Comment',        (r'(//|#).*',)),                                    # NOQA
-        ('NL',             (r'[\r\n]+',)),                                     # NOQA
-        ('Space',          (r'[ \t\r\n]+',)),                                  # NOQA
-        ('RackItem',       (r':[^\r\n\[]+',)),                                 # NOQA
-        ('NonnumRackItem', (r'[\*\-]\s*[^\r\n\[]+',)),                         # NOQA
-        ('Units',          (r'([0-9]+U|[0-9]+(?:\.[0-9]+)?(A|kg))',)),         # NOQA
-        ('Number',         (r'[0-9]+',)),                                      # NOQA
-        ('Name',           (u('[A-Za-z_0-9\u0080-\uffff]') +                   # NOQA
-                            u('[A-Za-z_\\-.0-9\u0080-\uffff]*'),)),            # NOQA
-        ('Op',             (r'[{}:;,=\[\]]',)),                                # NOQA
-        ('String',         (r'(?P<quote>"|\').*?(?<!\\)(?P=quote)', DOTALL)),  # NOQA
+    specs = [                                                            # NOQA
+        ('Comment',  (r'/\*(.|[\r\n])*?\*/', MULTILINE)),                # NOQA
+        ('Comment',  (r'(//|#).*',)),                                    # NOQA
+        ('NL',       (r'[\r\n]+',)),                                     # NOQA
+        ('RackItem', (r'(?<=[:*\-])[^\r\n\[;]+',)),                      # NOQA
+        ('Space',    (r'[ \t\r\n]+',)),                                  # NOQA
+        ('Units',    (r'([0-9]+U|[0-9]+(?:\.[0-9]+)?(A|kg))',)),         # NOQA
+        ('Number',   (r'[0-9]+',)),                                      # NOQA
+        ('Name',     (u('[A-Za-z_0-9\u0080-\uffff]') +                   # NOQA
+                      u('[A-Za-z_\\-.0-9\u0080-\uffff]*'),)),            # NOQA
+        ('Op',       (r'[{}:;,*\-=\[\]]',)),                             # NOQA
+        ('String',   (r'(?P<quote>"|\').*?(?<!\\)(?P=quote)', DOTALL)),  # NOQA
     ]
     useless = ['Comment', 'NL', 'Space']
     t = make_tokenizer(specs)
@@ -81,90 +79,132 @@ def tokenize(string):
 
 def parse(seq):
     """Sequence(Token) -> object"""
-    unarg = lambda f: lambda args: f(*args)
+    id_tokens = ['Name', 'Number', 'String', 'Units']
+
     tokval = lambda x: x.value
-    flatten = lambda list: sum(list, [])
-    n = lambda s: a(Token('Name', s)) >> tokval
     op = lambda s: a(Token('Op', s)) >> tokval
     op_ = lambda s: skip(op(s))
-    _id = some(lambda t: t.type in ['Name', 'Number', 'String', 'Units']
-               ).named('id') >> tokval
+    _id = some(lambda t: t.type in id_tokens) >> tokval
+    keyword = lambda s: a(Token('Name', s)) >> tokval
     number = some(lambda t: t.type == 'Number').named('number') >> tokval
     rackitem = some(lambda t: t.type == 'RackItem').named('rackitem') >> tokval
-    nonnum_rackitem = (some(lambda t: t.type == 'NonnumRackItem').
-                       named('nonnum_rackitem') >> tokval)
-    make_graph_attr = lambda args: DefAttrs(u('graph'), [Attr(*args)])
 
-    racklabel = lambda text: re.sub("^.\s*(.*?)\s*;?$", "\\1", text)
-    make_rackitem = lambda no, text, attr: RackItem(no, racklabel(text), attr)
-    make_rackitem2 = lambda text, attr: RackItem(None, racklabel(text), attr)
+    def make_num_rackitem(num, text, attr):
+        return RackItem(num, text.strip(), attr)
 
-    a_list = (
+    def make_nonnum_rackitem(text, attr):
+        return RackItem(None, text.strip(), attr)
+
+    #
+    # parts of syntax
+    #
+    option_stmt = (
         _id +
-        maybe(op_('=') + _id) +
-        skip(maybe(op(',')))
-        >> unarg(Attr))
-    attr_list = (
-        many(op_('[') + many(a_list) + op_(']'))
-        >> flatten)
-    graph_attr = _id + op_('=') + _id >> make_graph_attr
-    numbered_rackitem_stmt = (
+        maybe(op_('=') + _id)
+        >> create_mapper(Attr)
+    )
+    option_list = (
+        maybe(op_('[') + option_stmt + many(op_(',') + option_stmt) + op_(']'))
+        >> create_mapper(oneplus_to_list, default_value=[])
+    )
+
+    #  attributes statement::
+    #     default_shape = box;
+    #     default_fontsize = 16;
+    #
+    attribute_stmt = (
+        _id + op_('=') + _id
+        >> create_mapper(Attr)
+    )
+
+    #  field statement::
+    #     1: A
+    #     2: B [attr = value, attr = value];
+    #     * C [attr = value, attr = value];
+    #     * D [attr = value, attr = value];
+    #
+    numbered_field_item_stmt = (
         number +
+        op_(':') +
         rackitem +
-        attr_list
-        >> unarg(make_rackitem))
-    nonnumbered_rackitem_stmt = (
-        nonnum_rackitem +
-        attr_list
-        >> unarg(make_rackitem2))
-    rackitem_stmt = (
-        numbered_rackitem_stmt
-        | nonnumbered_rackitem_stmt)
+        option_list
+        >> create_mapper(make_num_rackitem)
+    )
+    nonnumbered_field_item_stmt = (
+        (op_('-') | op_('*')) +
+        rackitem +
+        option_list
+        >> create_mapper(make_nonnum_rackitem)
+    )
+    field_item_stmt = (
+        numbered_field_item_stmt |
+        nonnumbered_field_item_stmt
+    )
+
+    #  rack statement::
+    #     rack {
+    #       1: A;
+    #     }
+    #
 
     # rack definition
+    rack_inline_stmt = (
+        attribute_stmt |
+        field_item_stmt
+    )
+    rack_inline_stmt_list = (
+        many(rack_inline_stmt + skip(maybe(op(';'))))
+    )
     rack_stmt = (
-        rackitem_stmt
-        | a_list
-        | graph_attr
-    )
-    rack_stmt_list = many(rack_stmt + skip(maybe(op(';'))))
-    rack = (
-        skip(n('rack')) +
+        skip(keyword('rack')) +
         maybe(_id) +
         op_('{') +
-        rack_stmt_list +
+        rack_inline_stmt_list +
         op_('}')
-        >> unarg(Rack))
+        >> create_mapper(Rack)
+    )
 
-    # plugin definition
-    plugin_stmt = (
-        skip(n('plugin')) +
+    #  extension statement (plugin)::
+    #     plugin attributes [name = Name];
+    #
+    extension_stmt = (
+        keyword('plugin') +
         _id +
-        attr_list
-        >> unarg(AttrPlugin))
-
-    stmt = (
-        rack
-        | rackitem_stmt
-        | plugin_stmt
-        | a_list
+        option_list
+        >> create_mapper(Extension)
     )
-    stmt_list = many(stmt + skip(maybe(op(';'))))
-    graph = (
-        maybe(n('diagram') | n('rackdiag')) +
+
+    #
+    # diagram statement::
+    #     rackdiag {
+    #        A;
+    #     }
+    #
+    diagram_inline_stmt = (
+        extension_stmt |
+        rack_stmt |
+        field_item_stmt |
+        attribute_stmt
+    )
+    diagram_inline_stmt_list = (
+        many(diagram_inline_stmt + skip(maybe(op(';'))))
+    )
+    diagram = (
+        maybe(keyword('diagram') | keyword('rackdiag')) +
         maybe(_id) +
         op_('{') +
-        stmt_list +
+        diagram_inline_stmt_list +
         op_('}')
-        >> unarg(Graph))
-    dotfile = graph + skip(finished)
+        >> create_mapper(Diagram)
+    )
+    dotfile = diagram + skip(finished)
 
     return dotfile.parse(seq)
 
 
 def sort_tree(tree):
     def weight(node):
-        if isinstance(node, (Attr, DefAttrs, AttrPlugin)):
+        if isinstance(node, (Attr, Extension)):
             return 1
         else:
             return 2
