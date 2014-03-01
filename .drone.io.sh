de89f8442d31e4c90904658e20e7fbef53c443d4
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2014-03-01 17:31:57 +0900

    Add .drone.io.sh

---
+++ .drone.io.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+sudo add-apt-repository ppa:fkrull/deadsnakes
+sudo apt-get update
+sudo apt-get install python2.6 python2.6-dev fonts-ipafont-gothic fonts-vlgothic libjpeg8-dev libfreetype6-dev
+
+mkdir src/nwdiag/tests/truetype
+cp /usr/share/fonts/truetype/vlgothic/VL-PGothic-Regular.ttf src/nwdiag/tests/truetype
+
+pip install --use-mirrors --upgrade detox misspellings
+find src/ -name "*.py" | misspellings -f -
+detox
