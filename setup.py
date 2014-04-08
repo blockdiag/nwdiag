6d448ee2c6488167d359a43099f8de6f1d6ee09d
Takeshi KOMIYA <748828+tk0miya@users.noreply.github.com>
2014-04-08 16:28:38 +0900

    Revert tox.ini

--- setup.py
+++ setup.py
@@ -14,7 +14,10 @@ classifiers = [
     "Topic :: Software Development :: Documentation",
     "Topic :: Text Processing :: Markup",
 ]
-test_requires = ['nose', 'pep8>=1.3', 'docutils']
+test_requires = ['nose',
+                 'pep8>=1.3',
+                 'reportlab',
+                 'docutils']
 
 # only for Python2.6
 if sys.version_info > (2, 6) and sys.version_info < (2, 7):
