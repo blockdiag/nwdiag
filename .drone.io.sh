4cf671499334db48f80918c74ff19eaa7f754dac
tk0miya <i.tkomiya@gmail.com>
2014-11-15 15:53:19 +0900

    Avoid hard linking error on drone.io

--- .drone.io.sh
+++ .drone.io.sh
@@ -3,6 +3,12 @@ sudo add-apt-repository ppa:fkrull/deadsnakes
 sudo apt-get update
 sudo apt-get install python2.6 python2.6-dev python3.4 python3.4-dev fonts-ipafont-gothic libjpeg8-dev libfreetype6-dev
 
+# avoid hard linking errors (see http://bugs.python.org/issue8876)
+rm src/rackdiag/tests/VLGothic/VL-Gothic-Regular.ttf
+rm src/packetdiag/tests/VLGothic/VL-Gothic-Regular.ttf
+cp src/nwdiag/tests/VLGothic/VL-Gothic-Regular.ttf src/rackdiag/tests/VLGothic/
+cp src/nwdiag/tests/VLGothic/VL-Gothic-Regular.ttf src/packetdiag/tests/VLGothic/
+
 pip install --use-mirrors --upgrade detox misspellings check-manifest docutils
 find src/ -name "*.py" | misspellings -f -
 ALL_TESTS=1 detox
